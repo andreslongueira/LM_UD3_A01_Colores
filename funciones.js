55e0af29ffb95e1287c673528d6cb670e9b5052b
andreslongueira <73697133+andreslongueira@users.noreply.github.com>
2021-02-03 22:03:28 +0100

    JavaScript Arreglo

--- funciones.js
+++ funciones.js
@@ -46,3 +46,54 @@
     var intervalo = setInterval(actualizarHora, 1000);
 
 }())
+
+function cambiaColor(id){
+
+    var color= [];
+    color.push("#4a235a");
+    color.push("#1a5276");
+    color.push("#2874a6");
+    color.push("#148f77");
+    color.push("#138d75");
+    color.push("#f5b041");
+    color.push("#00FF00");
+    color.push("#0000FF");
+    color.push("#FF0000");
+    color.push("#00FFFF");
+
+    var todosIguales = 0;
+
+    var aleatorio = Math.round(Math.random()*9);
+    document.getElementById(id).style.backgroundColor = color[aleatorio];4
+
+
+    var sum = 0;
+    var bloques =[]
+    bloque= document.getElementById(bloque1).style.background;
+    bloque= document.getElementById(bloque2).style.background;
+    bloque= document.getElementById(bloque3).style.background;
+    bloque= document.getElementById(bloque4).style.background;
+    bloque= document.getElementById(bloque5).style.background;
+    bloque= document.getElementById(bloque6).style.background;
+    bloque= document.getElementById(bloque7).style.background;
+    bloque= document.getElementById(bloque8).style.background;
+    bloque= document.getElementById(bloque9).style.background;
+    bloque= document.getElementById(bloque10).style.background;
+
+for (let index = 0; index < bloques.length - 1; index++) {
+    
+    if(bloques[index] == bloques[index]){
+        sum++;
+    }
+    if(index == 8){
+        if(bloques[9] == bloques[0]){
+            sum++;
+        }
+    }
+    if(sum == 9){
+        alert("asdfghjklñ");
+    }
+    
+}
+}
+
